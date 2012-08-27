7942dd35823f2b686f71184e38588c6f696a5704
Alexander Auritt <alexauritt@gmail.com>
2012-08-27 12:46:45 -0400

    moved synch job to new file

--- lib/syncher.rb
+++ lib/syncher.rb
@@ -1,4 +1,5 @@
 require 'world_bank'
+require 'syncher/synch_job'
 require 'syncher/query_scheduler'
 
 CURRENT_INDICATOR = 'SP.POP.TOTL'
@@ -7,34 +8,4 @@ module Syncher
   CURRENT_INDICATOR = 'SP.POP.TOTL'
   MAXIMUM_BUFFER_SIZE = 500
   DEFAULT_INITIAL_BUFFER_SIZE = 50
-  class SynchJob
-    def self.do_it
-      puts "about to make wb api call..."
-      query = WorldBank::Data.country('all').indicator(CURRENT_INDICATOR)
-      data = query.fetch
-      available_data_count = query.total
-      puts "There are #{available_data_count} pieces of data available"
-      if available_data_count > Syncher::DEFAULT_INITIAL_BUFFER_SIZE
-        fetch_all_data(query)
-      else
-        complete
-      end
-    end
-
-    def self.fetch_all_data(query)
-      scheduler = Syncher::QueryScheduler.new(query)
-      query.per_page(Syncher::MAXIMUM_BUFFER_SIZE)
-      results = scheduler.execute!
-      if results
-        puts "results returned"
-      else
-        checksum = Digest::MD5.hexdigest Marshal.dump(results)
-        puts "results returned, with checksum: #{checksum}"
-      end
-    end
-
-    def self.complete
-      puts "Data fetch complete."
-    end
-  end
 end
