73fb593bc8448292c6b2ea5e5742b0b0e0812b2b
Alexander Auritt <alexauritt@gmail.com>
2012-08-27 17:43:03 -0400

    data parser ensures that all data is from same indicator

--- lib/syncher/data_parser.rb
+++ lib/syncher/data_parser.rb
@@ -3,8 +3,10 @@ require 'world_bank'
 module Syncher
   class DataParser
     def self.parse(data_collection)
-      results = {:id => data_collection.first.id }
+      indicator_id = data_collection.first.id
+      results = {:id => indicator_id }
       data_collection.each do |datum|
+        return nil unless datum.id === indicator_id
         results[datum.others['country']['value']] ||= []
         results[datum.others['country']['value']] << { :year => datum.date, :value => datum.value }
       end
--- spec/data_parser_spec.rb
+++ spec/data_parser_spec.rb
@@ -2,11 +2,26 @@ require 'helper'
 
 describe Syncher::DataParser do
   let(:indicator_id) { 'DJ.JIE.JIDL' }
-  let(:data) { [mock_data(:country => 'Mexico', :year => 1990, :id => indicator_id, :value => 234)] }
+  let(:datum1) { mock_data(:country => 'Mexico', :year => 1990, :id => indicator_id, :value => 234) }
+  let(:data) { [datum1] }
 
   it "should parse data and return expected hash structure" do
     actual = Syncher::DataParser.parse data
     actual.should eq({:id => indicator_id, 'Mexico' => [{:year => 1990, :value => 234}]})
+  end
+  
+  it "should return nil when provided data for multiple indicators" do
+    datum2 = mock_data(:country => 'Mexico', :year => 1990, :id => 'NJ.EKD.DKEL', :value => 234)
+    Syncher::DataParser.parse([datum1, datum2]).should be_nil
+  end
+  
+  it "should handle more complext data" do
+    d1 = mock_data(:country => 'Mexico', :year => 1990, :id => indicator_id, :value => 234)
+    d2 = mock_data(:country => 'Mexico', :year => 1992, :id => indicator_id, :value => 456)
+    d3 = mock_data(:country => 'France', :year => 1990, :id => indicator_id, :value => 789)
+    expected = {:id => indicator_id, 'Mexico' => [{:year => 1990, :value => 234}, {:year => 1992, :value => 456}], 'France' => [{:year => 1990, :value => 789}]}
+
+    Syncher::DataParser.parse([d1,d2,d3]).should eq(expected)    
   end
 
   it "should mock data properly" do
